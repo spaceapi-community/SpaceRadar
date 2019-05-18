40a3893b99bedb049e2b1f56a0975653152b299a
gidsi <gidsi@chaospott.de>
2019-05-18 20:13:37 +0200

    Use filestorage instead of regular key/value

--- store/index.js
+++ store/index.js
@@ -1,11 +1,11 @@
 import { persistCombineReducers } from 'redux-persist'
 import { reducer as spaces } from './spaces';
-import {AsyncStorage} from "react-native";
+import ExpoFileSystemStorage from "redux-persist-expo-filesystem"
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage,
+  storage: ExpoFileSystemStorage,
   stateReconciler: autoMergeLevel2,
 };
 
